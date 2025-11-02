8273b48c3e4ee159888fd6e03324fb2b535a636f
GiorgosTarnaras <giorgostarn2612@gmail.com>
2025-11-02 15:50:10 +0200

    correct

--- lab1/EX1/ex1.vhd
+++ lab1/EX1/ex1.vhd
@@ -6,6 +6,11 @@ entity array_slices_x1D is
 		row1: in  std_logic_vector(3 downto 0);
 		row2: in  std_logic_vector(3 downto 0);
 		row3: in  std_logic_vector(3 downto 0);
+		ctrl1_R: in integer range 1 to 3;
+		ctrl1_C: in integer range 0 to 3;
+		ctrl2: in integer range 1 to 3;
+		ctrl3: in integer range 1 to 3;
+		ctrl4: in integer range 0 to 3;
 		slice1: out std_logic;
 		slice2: out std_logic_vector(1 downto 0);
 		slice3: out std_logic_vector(3 downto 0);
@@ -19,9 +24,10 @@ architecture arch of array_slices_x1D is
 type type_1Dx1D is array (1 to 3) of std_logic_vector(3 downto 0); 
 signal my_array: type_1Dx1D;
 begin 
+
 	my_array <= (row1, row2, row3);
-	slice1 <= my_array(1)(3);
-	slice2 <= my_array(2)(3 downto 2);
-	slice3 <= my_array(3);
-	slice4 <= my_array(1)(0) & my_array(2)(0) & my_array(3)(0);
+	slice1 <= my_array(ctrl1_R)(ctrl1_C);
+	slice2 <= my_array(ctrl2)(3 downto 2);
+	slice3 <= my_array(ctrl3);
+	slice4 <= my_array(1)(ctrl4) & my_array(2)(ctrl4) & my_array(3)(ctrl4);
 end arch;
--- lab1/EX1/ex1_B.vhd
+++ lab1/EX1/ex1_B.vhd
@@ -6,6 +6,11 @@ entity array_slices_2D is
 		row1: in  std_logic_vector(3 downto 0);
 		row2: in  std_logic_vector(3 downto 0);
 		row3: in  std_logic_vector(3 downto 0);
+		ctrl1_R: in integer range 1 to 3;
+		ctrl1_C: in integer range 0 to 3;
+		ctrl2: in integer range 1 to 3;
+		ctrl3: in integer range 1 to 3;
+		ctrl4: in integer range 0 to 3;
 		slice1: out std_logic;
 		slice2: out std_logic_vector(1 downto 0);
 		slice3: out std_logic_vector(3 downto 0);
@@ -16,25 +21,25 @@ end array_slices_2D;
 
 architecture arch of array_slices_2D is 
 
-type type_2D is array (0 to 2, 3 downto 0) of std_logic; 
+type type_2D is array (1 to 3, 3 downto 0) of std_logic; 
 signal my_array: type_2D;
 
 begin 
-	my_array(0, 3) <= row1(3);
-	my_array(0, 2) <= row1(2);
-	my_array(0, 1) <= row1(1);
-	my_array(0, 0) <= row1(0);
-	my_array(1, 3) <= row2(3);
-	my_array(1, 2) <= row2(2);
-	my_array(1, 1) <= row2(1);
-	my_array(1, 0) <= row2(0);
-	my_array(2, 3) <= row3(3);
-	my_array(2, 2) <= row3(2);
-	my_array(2, 1) <= row3(1);
-	my_array(2, 0) <= row3(0);
+	my_array(1, 3) <= row1(3);
+	my_array(1, 2) <= row1(2);
+	my_array(1, 1) <= row1(1);
+	my_array(1, 0) <= row1(0);
+	my_array(2, 3) <= row2(3);
+	my_array(2, 2) <= row2(2);
+	my_array(2, 1) <= row2(1);
+	my_array(2, 0) <= row2(0);
+	my_array(3, 3) <= row3(3);
+	my_array(3, 2) <= row3(2);
+	my_array(3, 1) <= row3(1);
+	my_array(3, 0) <= row3(0);
 	
-	slice1 <= my_array(0, 3);
-	slice2 <= my_array(1, 3) & my_array(1, 2);
-	slice3 <= my_array(2, 3) & my_array(2, 2) & my_array(2, 1) & my_array(2, 0);
-	slice4 <= my_array(0, 0) & my_array(1, 0) & my_array(2, 0);
+	slice1 <= my_array(ctrl1_R, ctrl1_C);
+	slice2 <= my_array(ctrl2, 3) & my_array(ctrl2, 2);
+	slice3 <= my_array(ctrl3, 3) & my_array(ctrl3, 2) & my_array(ctrl3, 1) & my_array(ctrl3, 0);
+	slice4 <= my_array(1, ctrl4) & my_array(2, ctrl4) & my_array(3, ctrl4);
 end arch;

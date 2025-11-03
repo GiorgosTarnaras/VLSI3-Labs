8c2d31ecd00e6ccf02d86d5280f347f099ec1f5c
kostisBN <kostass840@gmail.com>
2025-11-03 12:56:56 +0200

    Removed parenthesys

--- lab1/Ex_2/ex2_b.vhd
+++ lab1/Ex_2/ex2_b.vhd
@@ -12,81 +12,80 @@ end entity exercise_2;
 
 architecture arch2 of exercise_2 is
 begin
-
     active <= '0' when r = "00000000" else '1';
-
-    code <=
-
-        ("000" when r(0) = '1' else
-         "111" when r(7) = '1' else
-         "110" when r(6) = '1' else
-         "101" when r(5) = '1' else
-         "100" when r(4) = '1' else
-         "011" when r(3) = '1' else
-         "010" when r(2) = '1' else
-         "001" when r(1) = '1' else "000") when c = "000" else
-
-        ("001" when r(1) = '1' else
-         "000" when r(0) = '1' else
-         "111" when r(7) = '1' else
-         "110" when r(6) = '1' else
-         "101" when r(5) = '1' else
-         "100" when r(4) = '1' else
-         "011" when r(3) = '1' else
-         "010" when r(2) = '1' else "000") when c = "001" else
-
-        ("010" when r(2) = '1' else
-         "001" when r(1) = '1' else
-         "000" when r(0) = '1' else
-         "111" when r(7) = '1' else
-         "110" when r(6) = '1' else
-         "101" when r(5) = '1' else
-         "100" when r(4) = '1' else
-         "011" when r(3) = '1' else "000") when c = "010" else
-
-        ("011" when r(3) = '1' else
-         "010" when r(2) = '1' else
-         "001" when r(1) = '1' else
-         "000" when r(0) = '1' else
-         "111" when r(7) = '1' else
-         "110" when r(6) = '1' else
-         "101" when r(5) = '1' else
-         "100" when r(4) = '1' else "000") when c = "011" else
-
-        ("100" when r(4) = '1' else
-         "011" when r(3) = '1' else
-         "010" when r(2) = '1' else
-         "001" when r(1) = '1' else
-         "000" when r(0) = '1' else
-         "111" when r(7) = '1' else
-         "110" when r(6) = '1' else
-         "101" when r(5) = '1' else "000") when c = "100" else
-
-        ("101" when r(5) = '1' else
-         "100" when r(4) = '1' else
-         "011" when r(3) = '1' else
-         "010" when r(2) = '1' else
-         "001" when r(1) = '1' else
-         "000" when r(0) = '1' else
-         "111" when r(7) = '1' else
-         "110" when r(6) = '1' else "000") when c = "101" else
-
-        ("110" when r(6) = '1' else
-         "101" when r(5) = '1' else
-         "100" when r(4) = '1' else
-         "011" when r(3) = '1' else
-         "010" when r(2) = '1' else
-         "001" when r(1) = '1' else
-         "000" when r(0) = '1' else
-         "111" when r(7) = '1' else "000") when c = "110" else
-
-        ("111" when r(7) = '1' else
-         "110" when r(6) = '1' else
-         "101" when r(5) = '1' else
-         "100" when r(4) = '1' else
-         "011" when r(3) = '1' else
-         "010" when r(2) = '1' else
-         "001" when r(1) = '1' else
-         "000" when r(0) = '1' else "000");
-
+    
+    code <= "000" when c = "000" and r(0) = '1' else
+            "111" when c = "000" and r(7) = '1' else
+            "110" when c = "000" and r(6) = '1' else
+            "101" when c = "000" and r(5) = '1' else
+            "100" when c = "000" and r(4) = '1' else
+            "011" when c = "000" and r(3) = '1' else
+            "010" when c = "000" and r(2) = '1' else
+            "001" when c = "000" and r(1) = '1' else
+            
+            "001" when c = "001" and r(1) = '1' else
+            "000" when c = "001" and r(0) = '1' else
+            "111" when c = "001" and r(7) = '1' else
+            "110" when c = "001" and r(6) = '1' else
+            "101" when c = "001" and r(5) = '1' else
+            "100" when c = "001" and r(4) = '1' else
+            "011" when c = "001" and r(3) = '1' else
+            "010" when c = "001" and r(2) = '1' else
+            
+            "010" when c = "010" and r(2) = '1' else
+            "001" when c = "010" and r(1) = '1' else
+            "000" when c = "010" and r(0) = '1' else
+            "111" when c = "010" and r(7) = '1' else
+            "110" when c = "010" and r(6) = '1' else
+            "101" when c = "010" and r(5) = '1' else
+            "100" when c = "010" and r(4) = '1' else
+            "011" when c = "010" and r(3) = '1' else
+            
+            "011" when c = "011" and r(3) = '1' else
+            "010" when c = "011" and r(2) = '1' else
+            "001" when c = "011" and r(1) = '1' else
+            "000" when c = "011" and r(0) = '1' else
+            "111" when c = "011" and r(7) = '1' else
+            "110" when c = "011" and r(6) = '1' else
+            "101" when c = "011" and r(5) = '1' else
+            "100" when c = "011" and r(4) = '1' else
+            
+            "100" when c = "100" and r(4) = '1' else
+            "011" when c = "100" and r(3) = '1' else
+            "010" when c = "100" and r(2) = '1' else
+            "001" when c = "100" and r(1) = '1' else
+            "000" when c = "100" and r(0) = '1' else
+            "111" when c = "100" and r(7) = '1' else
+            "110" when c = "100" and r(6) = '1' else
+            "101" when c = "100" and r(5) = '1' else
+            
+            "101" when c = "101" and r(5) = '1' else
+            "100" when c = "101" and r(4) = '1' else
+            "011" when c = "101" and r(3) = '1' else
+            "010" when c = "101" and r(2) = '1' else
+            "001" when c = "101" and r(1) = '1' else
+            "000" when c = "101" and r(0) = '1' else
+            "111" when c = "101" and r(7) = '1' else
+            "110" when c = "101" and r(6) = '1' else
+            
+            "110" when c = "110" and r(6) = '1' else
+            "101" when c = "110" and r(5) = '1' else
+            "100" when c = "110" and r(4) = '1' else
+            "011" when c = "110" and r(3) = '1' else
+            "010" when c = "110" and r(2) = '1' else
+            "001" when c = "110" and r(1) = '1' else
+            "000" when c = "110" and r(0) = '1' else
+            "111" when c = "110" and r(7) = '1' else
+            
+            "111" when c = "111" and r(7) = '1' else
+            "110" when c = "111" and r(6) = '1' else
+            "101" when c = "111" and r(5) = '1' else
+            "100" when c = "111" and r(4) = '1' else
+            "011" when c = "111" and r(3) = '1' else
+            "010" when c = "111" and r(2) = '1' else
+            "001" when c = "111" and r(1) = '1' else
+            "000" when c = "111" and r(0) = '1' else
+            
+            "000";
+            
 end architecture arch2;

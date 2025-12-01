69307253bf341ded32ec346e9e007b6ab10640dd
GiorgosTarnaras <giorgostarn2612@gmail.com>
2025-12-01 11:20:08 +0200

    corrected serial mean

--- lab4/ex3/ex3_serial.vhd
+++ lab4/ex3/ex3_serial.vhd
@@ -3,38 +3,36 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity mean_value is 
-	port (clk, rst: in std_logic;
-		  x: in std_logic_vector(15 downto 0);
-		  y: out std_logic_vector(15 downto 0));
+    port (clk, rst: in std_logic;
+          x: in std_logic_vector(15 downto 0);
+          y: out std_logic_vector(15 downto 0));
 end mean_value;
 
-
 architecture serial of mean_value is 
-signal accum, serial_in: std_logic_vector(15 downto 0);
-signal counter : integer range 0 to 8 := 0;
+    signal serial_in: std_logic_vector(15 downto 0);
+    signal accum, serial_out: std_logic_vector(18 downto 0);
+    signal cnt: integer range 0 to 8;
 begin 
-	process(clk, rst)
+    process(clk, rst)
     begin
-		if (rst = '1') then 
-			accum <= (others => '0');
-			serial_in <= (others => '0');
-			y <= (others => '0');
-			counter <= 0;
-		elsif (rising_edge(clk)) then	
-			
-			if counter = 8 then 
-				y <= accum;
-				counter <= 0;
-			else
-				counter <= counter + 1;
-			end if;
-
-			serial_in <= (15 => '0', 14 => '0', 13 => '0', others => x(12 downto 0));
-			accum <= std_logic_vector(unsigned(accum) + unsigned(serial_in));
-
-		end if;
-
-	end process;
-	
-	
+        if (rst = '1') then
+            serial_in <= (others => '0');
+            accum <= (others => '0');
+            cnt <= 0;
+            serial_out <= (others => '0');
+        
+        elsif (rising_edge(clk)) then 
+        	serial_in <= x;  
+            if cnt = 8 then 
+            	serial_out <= std_logic_vector(unsigned(accum) + unsigned(serial_in));
+            	cnt <= 1;
+            	accum <= (others => '0');
+            else
+                accum <= std_logic_vector(unsigned(accum) + unsigned(serial_in));
+                cnt <= cnt + 1;
+            end if;
+        end if;
+        y <= serial_out(18 downto 3);
+    end process;
+    
 end serial;

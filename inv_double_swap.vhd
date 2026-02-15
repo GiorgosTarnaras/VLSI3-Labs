f9c37ec7101fc5f10441f70bbbfac9b6aa0ff7b0
kostisBN <126887461+kostisBN@users.noreply.github.com>
2026-02-15 19:36:01 +0200

    Create inv_double_swap.vhd

---
+++ inv_double_swap.vhd
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity CLEFIA_INV_DOUBLE_SWAP is
+    Port ( input  : in  std_logic_vector (127 downto 0);
+           output : out std_logic_vector (127 downto 0));
+end CLEFIA_INV_DOUBLE_SWAP;
+
+architecture behavioral of CLEFIA_INV_DOUBLE_SWAP is
+begin    
+    output <= input(63 downto 57) & 
+              input(128 downto 71)    & 
+              input(56 downto 0) &
+              input(70 downto 64);			  
+
+end behavioral;

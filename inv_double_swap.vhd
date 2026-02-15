d515ce862508f18fe6baa7b87ca7f42dae3090b2
kostisBN <126887461+kostisBN@users.noreply.github.com>
2026-02-15 20:07:25 +0200

    Update inv_double_swap.vhd

--- inv_double_swap.vhd
+++ inv_double_swap.vhd
@@ -9,7 +9,7 @@ end CLEFIA_INV_DOUBLE_SWAP;
 architecture behavioral of CLEFIA_INV_DOUBLE_SWAP is
 begin    
     output <= input(63 downto 57) & 
-              input(128 downto 71)    & 
+              input(127 downto 71)    & 
               input(56 downto 0) &
               input(70 downto 64);			  
 
